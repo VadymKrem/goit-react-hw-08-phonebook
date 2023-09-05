a7dc4151b55fa34edb2c8990fa980657204a30a8
VadymKrem <vadimkuzin2@gmail.com>
2023-09-05 23:44:57 +0300

    first evening try
    make pages
    auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,65 @@
 import { Container } from './App.styled';
 
-import { Section } from './Section/Section';
-import { ContactList } from './ContactList/ContactList';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectError, selectIsLoading } from 'Redux/selectors';
-import { useEffect } from 'react';
-import { fetchContacts } from 'Redux/operations';
+// import { Section } from './Section/Section';
+// import { ContactList } from './ContactList/ContactList';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Filter } from './Filter/Filter';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectError, selectIsLoading } from 'Redux/selectors';
+import { lazy } from 'react';
+// import { fetchContacts } from 'Redux/operations';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation/Navigation';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+
+const HomePage = lazy(() => import('pages/Home/Home'));
+const RegisterPage = lazy(() => import('pages/Register/Register'));
+const LoginPage = lazy(() => import('pages/Login/Login'));
+const ContactsPage = lazy(() => import('pages/Contacts/Contacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
   return (
     <Container>
-      <Section title="Phonebook" />
-      <ContactForm />
-      <Section title="Contacts" />
-      <Filter />
-      {isLoading && !error && <p>Your request in progress</p>}
-      <ContactList />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/login"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
     </Container>
   );
 };
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Section } from 'components/Section/Section';
+import { ContentPageContainer } from './Home.styled';
+
+export const Home = () => {
+  return (
+    <ContentPageContainer>
+      <Section title="Welcome to PhoneBook" />
+    </ContentPageContainer>
+  );
+};
